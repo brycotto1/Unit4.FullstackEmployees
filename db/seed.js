3896ca5ccb7a7e5dfdb3149c75bafa34fe12852e
Bryan Cotto <brycotto1@yahoo.com>
2025-06-25 21:36:06 -0400

    Add functionality to db

--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { createEmployee } from "./queries/employees.js";
+import { faker } from "@faker-js/faker";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +8,11 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for(let i = 0; i < 10; i++){
+    await createEmployee({
+      name: faker.person.fullName(),
+      birthday: faker.date.birthdate(),
+      salary: faker.number.int({min:40000, max:100000})
+    });
+  }
 }
